6c4f070c22cfbc50c00617c17c799f84a9be9d3f
nickmullaney <nick.mullaney@gmail.com>
2023-05-17 12:23:48 -0700

    update

---
+++ src/routes/customer.js
@@ -0,0 +1,23 @@
+`use strict`;
+
+const express = require('express');
+
+const router = express.Router();
+const { customerModel } = require('./models');
+
+app.get('/customer', async (req, res, next) => {
+  let customers = await customerModel.findAll();
+
+  res.status(200).send(customers);
+});
+
+app.get('/customer/:id', async (req, res, next) => {
+  let singleCustomer = await customerModel.findAll({where: {id: req.params.id}});
+
+  res.status(200).send(singleCustomer);
+});
+
+app.post('/customer', async (req, res, next) => {
+  let newCustomer = await customerModel.create(req.body);
+  res.status(200).send(newCustomer);
+});
--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@
 
 const express = require('express');
 const cors = require('cors');
-const { customerModel } = require('./models');
+const customerRouter =  require('./routes/customer');
 
 const app = express();
 app.use(cors());
 // otherwise you wont see json in the body of the request
 app.use(express.json());
+app.use(customerRouter);
 // TODO get this working
 const customerRouter = require('./routes/customer');
 const { port } = require('pg/lib/defaults');
@@ -16,16 +17,7 @@ app.get('/', (req, res, next) => {
   res.status(200).send('Proof of life');
 })
 
-app.get('/customer', async (req, res, next) => {
-  let customers = await customerModel.findAll();
 
-  res.status(200).send(customers);
-});
-
-app.post('/customer', async (req, res, next) => {
-  let newCustomer = await customerModel.create(req.body);
-  res.status(200).send(newCustomer);
-});
 
 const start = (port) => { app.listen(port, () => console.log('Server running on ', port)) };
 
